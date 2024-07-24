f6a44fc56662936d97ebd1f6ba979ccb8a3de60c
shumani <shumanisimeli@gmail.com>
2024-07-24 13:08:09 +0200

    updated 1-fifo_cache.py

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" 1-fifo_cache module
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """Adds and retrieve data from dictionary
+    using FIFO algorithm
+    """
+    def __init__(self):
+        """Initializes variables"""
+        super().__init__()
+    
+    def put(self, key, item):
+        """Add a new item in the cache
+        """
+        if key and item:
+            if len(self.cache_data) == BaseCaching.MAX_ITEMS:
+                if key not in self.cache_data:
+                    discard_key = next(iter(self.cache_data))
+                    self.cache_data.pop(discard_key)
+                    print(f"DISCARD: {discard_key}")
+            self.cache_data[key] = item
+        
+    def get(self, key):
+        """ Retrieves item from cache
+        """
+        if not key or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
